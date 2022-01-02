7d9ab26e1e6321648b0a30281238c9c6fa13bb46
hlj <1342759887@qq.com>
2022-01-02 16:29:23 +0800

    增加hello接口测试test.http

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.jiawa.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @RequestMapping("/hello")
+
+    @GetMapping("/hello")
     public String hello(){
         return "hello world!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello post!" + name;
     }
 }
