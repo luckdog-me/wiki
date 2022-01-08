cc12b3fbd6c0d63f757521859bd2d11058c93448
hlj <1342759887@qq.com>
2022-01-08 21:42:41 +0800

    修正the-footer和the-header组件导入的错误

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -9,6 +9,6 @@
     import { defineComponent } from 'vue';
 
     export default defineComponent({
-        name: 'the-foot'
+        name: 'the-footer'
     });
 </script>
--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// @ts-ignore
 import Antd  from 'ant-design-vue';
 import "ant-design-vue/dist/antd.css";
 
